a13c4162b8d6b3491ce36a3f7bd0994f965bcdf0
Bekasyl <devbekasyl@gmail.com>
2024-11-01 15:44:04 +0500

    feat: star component

--- components/ui/RatingStar.vue
+++ components/ui/RatingStar.vue
@@ -11,7 +11,7 @@ defineProps<RatingStar>();
   <div class="flex py-2 items-center">
     <ul class="flex">
       <li v-for="index in 5" :key="index" class="text-yellow-500">
-        <NuxtImg src="/svg/star.svg" width="16" height="16" />
+        <NuxtImg src="/svg/16x16/star.svg" width="16" height="16" />
       </li>
     </ul>
     <span class="ml-3 text-gray-600 text-sm">({{ orderCount }})</span>
