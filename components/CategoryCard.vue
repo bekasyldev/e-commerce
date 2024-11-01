e229927689e6f57bdc19c1dfc850eecfd03baef3
Bekasyl <devbekasyl@gmail.com>
2024-11-01 16:08:02 +0500

    feat: category section

---
+++ components/CategoryCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface CategoryCard {
+  name: string;
+}
+
+defineProps<CategoryCard>();
+</script>
+
+<template>
+  <div
+    class="border border-gray-700 flex flex-col justify-center items-center w-[170px] h-[145px]"
+  >
+    <NuxtImg
+      :src="`/svg/56x56/${name}.svg`"
+      :alt="name"
+      width="56"
+      height="56"
+    />
+    <span>{{ name }}</span>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,7 @@
       :isLiked="true"
     />
   </div>
+  <Categories />
   <Features />
   <Footer />
 </template>
